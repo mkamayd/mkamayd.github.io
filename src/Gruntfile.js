dbbb29dcedfe82e37fa0a37570d9ffcf1be9d494
Miguel <kamayd@gmail.com>
2014-12-12 17:55:18 +0000

    don't remove the src folder or the git folder

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -138,7 +138,8 @@ module.exports = function (grunt) {
           src: [
             '.tmp',
             '<%= yeoman.dist %>/{,*/}*',
-            '!<%= yeoman.dist %>/.git{,*/}*'
+            '!<%= yeoman.dist %>/.git{,*/}*',
+            '!<%= yeoman.dist %>/src{,*/}*'
           ]
         }]
       },
