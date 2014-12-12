6e6665456398a1f9a4a750fd945c4bd9513b5815
Miguel <kamayd@gmail.com>
2014-12-12 20:33:01 +0000

    the dist files

---
+++ scripts/scripts.a5e88850.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("kamaydApp",["ngAnimate","ngCookies","ngResource","ngRoute","ngSanitize","ngTouch"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("kamaydApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
