35c97cd4193cc7fdb4547e4fea65679bfdf033d1
Miguel <kamayd@gmail.com>
2014-12-19 19:52:19 +0000

    fixing small bug in the skills page.

--- src/app/scripts/controllers/skills.js
+++ src/app/scripts/controllers/skills.js
@@ -1,18 +1,21 @@
 'use strict';
 angular.module('kamaydApp')
   .controller('SkillsCtrl', function ($scope, data, $stateParams) {
+
      $scope.categories = data.getCategories();
      $scope.selectedSkill = data.findSkill($stateParams.selected) || $scope.categories[0].skills[0];
+     $scope.filter = {
+         onlyWeapons : $scope.selectedSkill.weapon
+     };
      $scope.selectSkill = function(skill){
          if($scope.selectedSkill === skill){
              $scope.selectedSkill = null;
          }
-         else {$scope.selectedSkill = skill;
+         else {
+             $scope.selectedSkill = skill;
          }
      };
-     $scope.filter = {
-         onlyWeapons : true
-     };
+
      $scope.skillFilter = function(value){
          if($scope.filter.onlyWeapons)
          {
