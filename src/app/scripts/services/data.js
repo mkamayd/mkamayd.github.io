683a51175ef0b802ce11c640a498892423965e7e
mkamayd <kamayd@gmail.com>
2015-07-21 22:37:23 -0400

    Refactoring related with Members of a Company. Plus adding some more data.

--- src/app/scripts/services/data.js
+++ src/app/scripts/services/data.js
@@ -3,8 +3,11 @@
 
 angular.module('kamaydApp')
     .factory('data', function data() {
-        function m(date){
-            return moment(date, 'DD-MM-YYYY');
+        function m(date) {
+            if (date)
+                return moment(date, 'DD-MM-YYYY');
+            else
+                return moment();
         }
         function ImgLink(className, url){
             this.className = className;
@@ -19,13 +22,12 @@ angular.module('kamaydApp')
                 }
             );
         }
-        function Company(name, shortName, location, startDate, endDate, role, url, socialLinks, projects, team){
+        function Company(name, shortName, location, startDate, endDate, url, socialLinks, projects, team){
             this.name = name;
             this.shortName = shortName;
             this.location = location;
             this.startDate = startDate;
             this.endDate = endDate;
-            this.role = role;
             this.url = url;
             this.socialLinks = socialLinks;
             var thisCompany = this;
@@ -94,9 +96,8 @@ angular.module('kamaydApp')
             }
         };
 
-        function Person(name, title , shortName, linkedInURL){
+        function Person(name, shortName, linkedInURL){
             this.name = name;
-            this.title = title;
             this.shortName = shortName || 'person';
             this.linkedInURL = linkedInURL;
             if(!_.contains(this.all, this))
@@ -108,14 +109,19 @@ angular.module('kamaydApp')
             all:[]
         };
 
+        function Member(person, title) {
+            _.defaults(this, person);
+            this.position = title;
+        };
+
         function Education(location, startDate, endDate) {
             this.location = location;
             this.startDate = startDate;
             this.endDate = endDate;
         }
 
-        function Recomendation(person, company, text) {
-            this.person = person;
+        function Recomendation(member, company, text) {
+            this.member = member;
             this.company = company;
             this.text = text;
         }
@@ -149,25 +155,39 @@ angular.module('kamaydApp')
             git = new Skill('GIT', 'http://git-scm.com/', 'A Distributed version control system.', true),
 
             wpf = new Skill('WPF', 'http://msdn.microsoft.com/en-us/library/aa970268(v=vs.110).aspx','Windows Presentation Foundation is a .Net presentation system for building Windows applications.');
-        //members
-        var miguel = new Person('Miguel Gutierrez Kamayd', 'Senior Software Developer', 'migue' , 'https://www.linkedin.com/in/miguelgutierrezkamayd'),
-            catherine = new Person('Catherine Keble', 'Lead Systems Tester', 'catherine', 'https://www.linkedin.com/pub/catherine-keble/0/747/615'),
-            garth = new Person('Garth Hinkel', 'Software Development Manager', 'garth', 'https://www.linkedin.com/in/garthhinkel'),
-            dan = new Person('Dan Barua', 'Lead Developer', 'dan', 'https://www.linkedin.com/pub/dan-barua/30/442/89'),
-            ahmed = new Person('Ahmed Yaslem', 'Systems Tester', 'ahmed', 'https://www.linkedin.com/in/ayaslem'),
-            tom = new Person('Thomas Mutton', 'Software Developer', 'tom', 'https://www.linkedin.com/in/tmutton1'),
-            ro = new Person('Rohan Mohindra', 'Inside Pre-Sales Consultant', 'ro', 'https://www.linkedin.com/pub/rohan-mohindra/37/7a9/890'),
-            simon = new Person('Simon Bill', 'Lead Developer', 'simon', 'https://www.linkedin.com/pub/simon-bill/12/6b/b92');
+
+        //people
+        var miguel = new Person('Miguel Gutierrez Kamayd', 'migue' , 'https://www.linkedin.com/in/miguelgutierrezkamayd'),
+            catherine = new Person('Catherine Keble', 'catherine', 'https://www.linkedin.com/pub/catherine-keble/0/747/615'),
+            garth = new Person('Garth Hinkel', 'garth', 'https://www.linkedin.com/in/garthhinkel'),
+            dan = new Person('Dan Barua', 'dan', 'https://www.linkedin.com/pub/dan-barua/30/442/89'),
+            ahmed = new Person('Ahmed Yaslem', 'ahmed', 'https://www.linkedin.com/in/ayaslem'),
+            tom = new Person('Thomas Mutton', 'tom', 'https://www.linkedin.com/in/tmutton1'),
+            ro = new Person('Rohan Mohindra', 'ro', 'https://www.linkedin.com/pub/rohan-mohindra/37/7a9/890'),
+            simon = new Person('Simon Bill', 'simon', 'https://www.linkedin.com/pub/simon-bill/12/6b/b92');
+        //member
+        var mMiguelSrSoft = new Member(miguel, 'Senior Software Developer'),
+            mMiguelSrWeb = new Member(miguel, 'Senior Web Developer Consultant'),
+            mMiguelDev = new Member(miguel, 'Software Developer'),
+            mCatherine = new Member(catherine, 'Lead Systems Tester'),
+            mGarth = new Member(garth, 'Software Development Manager'),
+            mDan = new Member(dan, 'Lead Developer'),
+            mAhmed = new Member(ahmed, 'Systems Tester'),
+            mTom = new Member(tom, 'Jr Software Developer'),
+            mRo = new Member(ro, 'Inside Pre-Sales Consultant'),
+            mSimon = new Member(simon, 'Lead Developer');
+
         //projects
         var projectsBSL = [
-            new Project('WordWatch v5', [angular, grunt, bower, npm, sass, nancy, postgres, git], m('1-3-2014'), [miguel, garth, simon, dan, tom, catherine, ahmed], 'Latest digital call recording platform from BSL'),
-            new Project('Digivoice', [angular, redis, git], m('1-11-2012'), [miguel, garth, dan, catherine], 'This digital call recording platform provides an objective record of inbound and outbound phone activity which can be used to develop your team’s telephone communication skills.  It helps to identify areas where there is a clear training or coaching need as well as assessing the effectiveness of the system as a whole.','https://www.digivoice.co.uk'),
-            new Project('CODES', [aspnet, wpf, sql, svn], m('1-10-2012'), [miguel, garth, catherine], 'CODES is an interview recording product incorporating audio and video recording used by organisations conducting investigative interviews for evidence gathering. In the world of law enforcement, commercial investigations and disciplinary hearings audio&video recordings provide an accurate and impartial record to benefit all parties.'),
-            new Project('Callcraft', [sql, svn], m('1-1-2013'), [miguel, garth, dan, ro, catherine], 'OPEX Callcraft provides hosted telephony applications for general businesses and contact centres thereby relieving you of the need to install and maintain expensive capital equipment on your own premises.','http://www.opexhosting.co.uk/')
+            new Project('WordWatch v5', [angular, grunt, bower, npm, sass, nancy, postgres, git], m('1-3-2014'), [mMiguelSrSoft, mGarth, mSimon, mDan, mTom, mCatherine, mAhmed], 'Latest digital call recording platform from BSL'),
+            //new Project('Nats', [aspnet,wpf, svn], m('1-1-2013'), [mMiguelSrSoft, mGarth, mDan, mCatherine], 'nats interal project'),
+            new Project('Digivoice', [angular, redis, git], m('1-11-2012'), [mMiguelSrSoft, mGarth, mDan, mCatherine], 'This digital call recording platform provides an objective record of inbound and outbound phone activity which can be used to develop your team’s telephone communication skills.  It helps to identify areas where there is a clear training or coaching need as well as assessing the effectiveness of the system as a whole.', 'https://www.digivoice.co.uk'),
+            new Project('CODES', [aspnet, wpf, sql, svn], m('1-10-2012'), [mMiguelSrSoft, mGarth, mCatherine], 'CODES is an interview recording product incorporating audio and video recording used by organisations conducting investigative interviews for evidence gathering. In the world of law enforcement, commercial investigations and disciplinary hearings audio&video recordings provide an accurate and impartial record to benefit all parties.'),
+            new Project('Callcraft', [sql, svn], m('1-1-2013'), [mMiguelSrSoft, mGarth, mDan, mRo, mCatherine], 'OPEX Callcraft provides hosted telephony applications for general businesses and contact centres thereby relieving you of the need to install and maintain expensive capital equipment on your own premises.', 'http://www.opexhosting.co.uk/')
         ];
         var projectsICID = [
-         new Project('Galaxy Plus', [ wpf, sql, svn], m('1-09-2010'), [miguel], ' A system that provides centralized information obtained from different physiological parameters monitors in an intensive-care unit. This project focuses on the program that is running on a PC showing all the data and alarms obtained from each physiological parameters monitor in real time.'),
-         new Project('Continuous Monitoring of ECG signal', [aspnet, wpf, sql, svn], m('1-05-2011'), [miguel], 'A system for monitoring the electrocardiogram signal of a patient, while he performs normal life activities in order to make an accurate diagnosis. It consists of a device that captures ECG signal directly from the patient and sends it to his smartphone. The application on the phone can show that data like in an ECG graph paper, classify the signal and sends it to a server. It also has a website in which the obtained signal is analyzed by medical specialists who can diagnose and treat the patient by sending information back to his smartphone and a desktop application where the data in the smartphone can be upload and analyzed without the need of ever be send to the server mentioned before.')
+         new Project('Galaxy Plus', [wpf, sql, svn], m('1-09-2010'), [mMiguelDev], ' A system that provides centralized information obtained from different physiological parameters monitors in an intensive-care unit. This project focuses on the program that is running on a PC showing all the data and alarms obtained from each physiological parameters monitor in real time.'),
+         new Project('Continuous Monitoring of ECG signal', [aspnet, wpf, sql, svn], m('1-05-2011'), [mMiguelDev], 'A system for monitoring the electrocardiogram signal of a patient, while he performs normal life activities in order to make an accurate diagnosis. It consists of a device that captures ECG signal directly from the patient and sends it to his smartphone. The application on the phone can show that data like in an ECG graph paper, classify the signal and sends it to a server. It also has a website in which the obtained signal is analyzed by medical specialists who can diagnose and treat the patient by sending information back to his smartphone and a desktop application where the data in the smartphone can be upload and analyzed without the need of ever be send to the server mentioned before.')
         ];
         //categories
         var categories = [
@@ -179,17 +199,19 @@ angular.module('kamaydApp')
             new Category('Version control', [git, svn])
         ];
         //companies
-        var bsl = new Company('Business Systems', 'BSL', 'London, United Kingdom', m('19-03-2013'), m('2-01-2015'), 'Senior Software Developer', 'http://www.businesssystemsuk.co.uk/', [new ImgLink('si-linkedin', 'https://www.linkedin.com/company/business-systems-uk-ltd'), new ImgLink('si-twitter', 'https://twitter.com/BSLHQ'), new ImgLink('si-facebook', 'https://www.facebook.com/businessystemsuk')], projectsBSL, Person.prototype.all);
-        var icid = new Company('Central Institute of Digital Research', 'ICID', 'Havana, Cuba', m('1-09-2010'), m('1-09-2012'), 'Software Developer', 'http://www.ecured.cu/index.php/Instituto_Central_de_Investigaci%C3%B3n_Digital', [], projectsICID, []);
-        var companies = [ bsl, icid ];
+        var bsl = new Company('Business Systems', 'BSL', 'London, United Kingdom', m('19-03-2013'), m('2-01-2015'), 'http://www.businesssystemsuk.co.uk/', [new ImgLink('si-linkedin', 'https://www.linkedin.com/company/business-systems-uk-ltd'), new ImgLink('si-twitter', 'https://twitter.com/BSLHQ'), new ImgLink('si-facebook', 'https://www.facebook.com/businessystemsuk')], projectsBSL, [mMiguelSrSoft, mCatherine, mGarth, mDan, mAhmed, mTom, mRo, mSimon]);
+        var icid = new Company('Central Institute of Digital Research', 'ICID', 'Havana, Cuba', m('1-09-2010'), m('1-09-2012'), 'http://www.ecured.cu/index.php/Instituto_Central_de_Investigaci%C3%B3n_Digital', [], projectsICID, [mMiguelDev]);
+        var carnival = new Company('Carnival Cruise Lines', 'Carnival', 'Miami, U.S.A', m('27-07-2015'), m(), 'http://www.carnival.com/', [], [], [mMiguelSrWeb]);
+
+        var companies = [carnival, bsl, icid];
 
         //education
         var education = new Education('Havana, Cuba', m('1-09-2005'), m('5-07-2010'));
 
         //recomendations
         var recomendations = [
-            new Recomendation(catherine, bsl, 'I have worked with Miguel in the Development Team at Business Systems (UK) ltd for the past couple of years. Miguel is a resourceful and competent Senior Developer. He has worked on technically demanding projects and has demonstrated that he is capable of meeting and overcoming challenges that he faced. Miguel is an easily approachable, knowledgeable Senior Developer and he is always ready to help others. He is very dedicated and hard-working. His good work is always well recognized. It would be a pleasure to work with Miguel again in the future and I have no hesitation in recommending Miguel to potential employers.'),
-            new Recomendation(garth, bsl, 'Miguel is a quick learner. He has a strong computer science background, and easily extends this to learn new technologies. He has worked full stack, but with a leaning towards UI (via AngularJS). Miguel is a productive, valued member of our team and is willing to get his hands dirty for the teams benefit.')
+            new Recomendation(mCatherine, bsl, 'I have worked with Miguel in the Development Team at Business Systems (UK) ltd for the past couple of years. Miguel is a resourceful and competent Senior Developer. He has worked on technically demanding projects and has demonstrated that he is capable of meeting and overcoming challenges that he faced. Miguel is an easily approachable, knowledgeable Senior Developer and he is always ready to help others. He is very dedicated and hard-working. His good work is always well recognized. It would be a pleasure to work with Miguel again in the future and I have no hesitation in recommending Miguel to potential employers.'),
+            new Recomendation(mGarth, bsl, 'Miguel is a quick learner. He has a strong computer science background, and easily extends this to learn new technologies. He has worked full stack, but with a leaning towards UI (via AngularJS). Miguel is a productive, valued member of our team and is willing to get his hands dirty for the teams benefit.')
         ];
 
         //courses
